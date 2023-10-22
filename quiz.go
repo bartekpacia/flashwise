9f28d684603f816ccf6c23ef7e0cb0a89829f090
Bartek Pacia <barpac02@gmail.com>
2023-10-23 00:08:56 +0200

    quiz wip

--- quiz.go
+++ quiz.go
@@ -1,6 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type generateQuizRequest struct {
+	SetID uint64 `json:"flashcard_set_id"`
+}
+
+type generateQuizResponse struct{}
 
 func GenerateQuiz(w http.ResponseWriter, r *http.Request) {
 	userID, ok := r.Context().Value(ContextUserKey).(uint64)
@@ -9,7 +20,29 @@ func GenerateQuiz(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_ = userID
+	var body generateQuizRequest
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		http.Error(w, "failed to decode request body", http.StatusBadRequest)
+		return
+	}
+
+	var set FlashcardSet
+	err = db.Get(&set, "SELECT * FROM flashcard_sets WHERE id = ?", body.SetID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, fmt.Sprintf("set with ID %d does not exist\n", body.SetID), http.StatusNotFound)
+			return
+		} else {
+			http.Error(w, fmt.Sprintln("failed to execute query:", err), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	if set.AuthorID != userID {
+		http.Error(w, fmt.Sprintf("set with ID %d does not belong to user %d\n", body.SetID, userID), http.StatusNotFound)
+		return
+	}
 
 	http.Error(w, "Not implemented", http.StatusNotImplemented)
 }
